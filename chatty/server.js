62f5e928e436345f6cfc9e590aced054c577e850
Eric Anderson <splanderson@gmail.com>
2016-10-18 20:36:39 -0600

    added messages and time to chatty project

--- chatty/server.js
+++ chatty/server.js
@@ -1,1 +1,46 @@
+var express = require('express');
+var app = express()
+var bodyParser = require('body-parser');
 
+var messages = [];
+app.use(bodyParser.json());
+
+app.listen(8666, function(){
+  console.log('Listening on port 8666');
+});
+
+app.get('/', function( req, res ) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.post('/', function( req, res ) {
+  messages.push({
+    message: req.body.message,
+    time: new Date()
+  });
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.options('/', function(req, res, next){
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  }).send();
+ });
